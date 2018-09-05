5a93aa52e6a3567c84f1b58583ecb113e7a7a393
Tho Duy Nguyen <tho.duy.nguyen@desjardins.com>
2018-09-04 22:37:55 -0400

    Add code for Vault

--- orgs.tf
+++ orgs.tf
@@ -4,7 +4,6 @@
 provider "grafana" {
   url     = "http://localhost:3000"
   auth    = "admin:admin"
-  version = "~> 1.2"
 }
 
 resource "grafana_organization" "org" {
@@ -27,27 +26,47 @@ provider "cf" {
 resource "cf_org" "orgs" {
   count        = "${length(var.orgs)}"
   name         = "${var.orgs[count.index]}"
-  managers = ["admin"]
+  managers     = ["admin"]
 }
 resource "cf_space" "spaces" {
-  count = "${length(var.orgs)}"
-  org   = "${element(cf_org.orgs.*.id, count.index)}"
-  name  = "dev"
+  count      = "${length(var.orgs)}"
+  org        = "${element(cf_org.orgs.*.id, count.index)}"
+  name       = "dev"
   developers = [
     "${element(cf_user.users.*.id, count.index)}"
   ]
 }
 
 resource "random_string" "passwords" {
-  count = "${length(var.orgs)}"
-  length = 16
-  special = true
-  override_special = "/@\" "
+  count            = "${length(var.orgs)}"
+  length           = 20
+  special          = false
 }
 
 resource "cf_user" "users" {
-  count = "${length(var.orgs)}"
-  name = "${format("%s-ciuser",var.orgs[count.index])}"
+  count    = "${length(var.orgs)}"
+  name     = "${format("%s-ciuser",var.orgs[count.index])}"
   password = "${element(random_string.passwords.*.result, count.index)}"
 }
   
+#
+# Provider Vault
+#
+provider "vault" {
+}
+
+resource "vault_mount" "secret" {
+  path = "secret"
+  type = "generic"
+  description = "Demo mount"
+}
+
+resource "vault_generic_secret" "secrets" {
+  count = "${length(var.orgs)}"
+  path  = "${format("secret/%s-ciuser-password", var.orgs[count.index])}"
+  data_json = <<EOT
+{
+  "value":   "${element(random_string.passwords.*.result, count.index)}"
+}
+EOT
+}
